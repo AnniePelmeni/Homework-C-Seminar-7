9e1409da9b7f7d4263a9ace4d7dc9c73e6b80ef7
Anna <vitani2007@mail.ru>
2023-07-25 15:34:16 +0300

    Task 47 solved

---
+++ Task_47/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void Print2DMassive(double[,] massive)
+{
+    for(int i = 0; i < massive.GetLength(0); i++)
+    {
+        for(int j = 0; j < massive.GetLength(1); j++)
+        {
+            Console.Write($"{massive[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] Create2DMassive(int rows, int columns, double startValue, double endValue)
+{
+    double[,] massive = new double[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            massive[i, j] = Math.Round(new Random().NextDouble() * (endValue - startValue) + startValue, 2);
+        }
+    }
+    return massive;
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = GetInput("Введите количество строк массива: ");
+int n = GetInput("Введите количество столбцов массива: ");
+
+double[,] matrix = Create2DMassive(m, n, -9, 9);
+Print2DMassive(matrix);
